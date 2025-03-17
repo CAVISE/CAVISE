b7a0274a2e771cf5d71a82190eb7ef977d30decf
Grigory Khoroshilov <khorogri@gmail.com>
2025-03-17 19:54:48 +0300

    [setup] Chmod +x

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import logging
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
-# Настройка логгера
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -20,7 +19,7 @@ def clone_repo(repo_base, repo_name):
     if os.path.isdir(repo_name):
         logger.info(f"Repository {repo_name} already exists. Skipping.")
         return
-    
+
     logger.info(f"Cloning {repo_url}...")
     try:
         Repo.clone_from(repo_url, repo_name, recursive=True)
@@ -44,16 +43,13 @@ if __name__ == "__main__":
         logger.error(f"Error determining repo base URL: {e}")
         sys.exit(1)
 
-    # Формируем базовый URL
     repo_base = origin_url.rsplit("/", 1)[0] + "/"
     logger.info(f"Repo base URL: {repo_base}")
 
-    # Список репозиториев для клонирования
     all_repos = ["opencda", "artery", "scenario-manager"]
     repos = sys.argv[1:] if len(sys.argv) > 1 else all_repos
     logger.info(f"Repositories to process: {repos}")
 
-    # Клонируем каждый репозиторий
     for repo in repos:
         clone_repo(repo_base, repo)
 
